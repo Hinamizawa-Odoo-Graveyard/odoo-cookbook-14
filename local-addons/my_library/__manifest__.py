2d39e9c85a32e1b47437dc7d875953ffa83795a9
Hinamizawa <themoonbunnys@gmail.com>
2022-03-02 15:56:51 -0400

    Update __manifest__.py

--- local-addons/my_library/__manifest__.py
+++ local-addons/my_library/__manifest__.py
@@ -6,4 +6,7 @@
         A Book Assignment from Odoo CookBook 14.0
     """,
     'price': 0.99,
+    'category': 'Profile Management',
+    'company': 'SMART-OBC',
+    'website': 'https://github.com/alejandroatacho',
 }
