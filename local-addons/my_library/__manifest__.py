9f6fc02945ac99c55608d50e5704778d92bd1338
Hinamizawa <themoonbunnys@gmail.com>
2022-03-02 11:31:30 -0400

    Update __manifest__.py

--- local-addons/my_library/__manifest__.py
+++ local-addons/my_library/__manifest__.py
@@ -1,12 +1,2 @@
 {
-    'name': "My library",
-    'summary': "Manage books easily",
-    'description': """ 
- Manage Library
- ==============
- Description related to library.
- """,
-    'author': "Hinamizawa",
-    'website': "https://github.com/alejandroatacho",
-    'category': 'Training',
-}
+    'name': "My library"}
