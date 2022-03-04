387ad6af7955ca5c5fcaebe69e7becd039f12d44
Hinamizawa <themoonbunnys@gmail.com>
2022-03-04 12:40:23 -0400

    update

--- local-addons/my_library/__manifest__.py
+++ local-addons/my_library/__manifest__.py
@@ -14,7 +14,9 @@
     'version': '14.0.1.0',
     'depends': ['base'],
     'data': [
-            'views/library_book.xml'
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml'
     ],
     # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
     'demo': [
